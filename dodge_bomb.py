18755467a051bc0e7cba54879016dbc5e2ed0986
kanomata <c0a240731d@edu.teu.ac.jp>
2025-11-25 14:38:41 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -13,6 +13,18 @@ DELTA = {
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRectかばくだんRect
+    戻り値：タプル（横方向判定結果，縦方向判定結果）
+    画面内ならTrue, 画面外ならFalse
+    """
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return yoko, tate
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -51,7 +63,14 @@ def main():
                 sum_mv[1] += mv[1]  # 縦方向の移動量
 
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True,True):  # 画面外に出たら
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])  # 反転される
         screen.blit(kk_img, kk_rct)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:  # 横方向にはみ出ていたら
+            vx *= -1
+        if not tate:  # 縦方向にはみ出ていたら
+            vy *= -1
         bb_rct.move_ip(vx,vy)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
